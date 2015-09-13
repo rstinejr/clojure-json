2a0bc2ea1294d7e5a8e077d8cbdfe0ade1dd0ecb
R.H. Stine <bob@waltonstine.net>
2015-09-13 10:13:37 -0400

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject json-reader "0.1.0-SNAPSHOT"
+(defproject json-reader "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
