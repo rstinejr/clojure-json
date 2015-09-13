6b220e9db94e11f4cffa56d293d33cc848c26456
R.H. Stine <bob@waltonstine.net>
2015-09-13 07:50:07 -0400

    We have to get JSON name and attribute values from the parser.

--- src/json_reader/core.clj
+++ src/json_reader/core.clj
@@ -21,6 +21,10 @@
   [json-parser]
   (.close json-parser))
 
+(defn get-item-attribs
+  [jp token]
+  {:token (.toString token) :name (.getCurrentName jp)})
+
 (defn start-parser
   "Kick off a go-block to parse a character input stream or reader of JSON source."
   [src-stream]
@@ -29,10 +33,10 @@
     (async/go-loop 
       [token (.nextToken jp)]
       (if token 
-        (do
+        (let [item-attribs (get-item-attribs jp token)]
           (when verbose
-            (println (str "token: " (.toString token))))
-          (async/put! token-chan token)
+            (println (str "item: " item-attribs)))
+          (async/put! token-chan item-attribs)
           (recur (.nextToken jp)))
         (do
          (async/close! token-chan)
