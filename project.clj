40fc095629093a3b8737ece26e3a94a28e4828c2
R.H. Stine <bob@waltonstine.net>
2015-09-14 20:28:45 -0400

    clean up reference to jackson-core-2.6.1.jar.

--- project.clj
+++ project.clj
@@ -4,10 +4,8 @@
   :license {:name "The MIT License (MIT)"
             :url "https://github.com/rstinejr/clojure-json/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure    "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :resource-paths ["resources/jars/jackson-annotations-2.6.1.jar" 
-                   "resources/jars/jackson-core-2.6.1.jar" 
-                   "resources/jars/jackson-databind-2.6.1.jar"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.fasterxml.jackson.core/jackson-core "2.6.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev { :plugins [[lein-marginalia "0.8.0"]]}})
